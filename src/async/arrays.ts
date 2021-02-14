c7bf6290a85c4e6108ceb68580283256bcef4a97
Ivan Dudinov <ivan@zajno.com>
2021-02-14 17:46:32 +0200

    async/arrays: allow to use non-extension functions

--- src/async/arrays.ts
+++ src/async/arrays.ts
@@ -1,17 +1,17 @@
 
 declare global {
     interface Array<T> {
-        someAsync(cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean>;
-        everyAsync(cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean>;
+        someAsync(this: T[], cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean>;
+        everyAsync(this: T[], cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean>;
 
-        forEachAsync(cb: (v: T, index?: number, arr?: T[]) => Promise<void>): Promise<void>;
-        mapAsync<R>(cb: (v: T, index?: number, arr?: T[]) => Promise<R>): Promise<R[]>;
+        forEachAsync(this: T[], cb: (v: T, index?: number, arr?: T[]) => Promise<void>): Promise<void>;
+        mapAsync<R>(this: T[], cb: (v: T, index?: number, arr?: T[]) => Promise<R>): Promise<R[]>;
     }
 }
 
-export async function someAsync<T>(this: T[], cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean> {
-    for (let i = 0; i < this.length; ++i) {
-        const ok = await cond(this[i], i, this);
+export async function someAsync<T>(target: T[], cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean> {
+    for (let i = 0; i < target.length; ++i) {
+        const ok = await cond(target[i], i, target);
         if (ok) {
             return true;
         }
@@ -20,9 +20,9 @@ export async function someAsync<T>(this: T[], cond: (v: T, index?: number, arr?:
     return false;
 }
 
-export async function everyAsync<T>(this: T[], cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean> {
-    for (let i = 0; i < this.length; ++i) {
-        const ok = await cond(this[i], i, this);
+export async function everyAsync<T>(target: T[], cond: (v: T, index?: number, arr?: T[]) => Promise<boolean>): Promise<boolean> {
+    for (let i = 0; i < target.length; ++i) {
+        const ok = await cond(target[i], i, target);
         if (!ok) {
             return false;
         }
@@ -31,24 +31,24 @@ export async function everyAsync<T>(this: T[], cond: (v: T, index?: number, arr?
     return true;
 }
 
-export async function forEachAsync<T>(this: T[], cb: (v: T, index?: number, arr?: T[]) => Promise<void>): Promise<void> {
-    for (let i = 0; i < this.length; ++i) {
-        await cb(this[i], i, this);
+export async function forEachAsync<T>(target: T[], cb: (v: T, index?: number, arr?: T[]) => Promise<void>): Promise<void> {
+    for (let i = 0; i < target.length; ++i) {
+        await cb(target[i], i, target);
     }
 }
 
-export async function mapAsync<T, R>(this: T[], cb: (v: T, index?: number, arr?: T[]) => Promise<R>): Promise<R[]> {
+export async function mapAsync<T, R>(target: T[], cb: (v: T, index?: number, arr?: T[]) => Promise<R>): Promise<R[]> {
     const results: R[] = [];
-    for (let i = 0; i < this.length; ++i) {
-        const r = await cb(this[i], i, this);
+    for (let i = 0; i < target.length; ++i) {
+        const r = await cb(target[i], i, target);
         results.push(r);
     }
     return results;
 }
 
 if (process.env.EXTEND_ARRAY_ASYNC) {
-    Array.prototype.someAsync = someAsync;
-    Array.prototype.everyAsync = everyAsync;
-    Array.prototype.forEachAsync = forEachAsync;
-    Array.prototype.mapAsync = mapAsync;
+    Array.prototype.someAsync = function(this, ...args) { return someAsync(this, ...args); };
+    Array.prototype.everyAsync = function(this, ...args) { return everyAsync(this, ...args); };
+    Array.prototype.forEachAsync = function(this, ...args) { return forEachAsync(this, ...args); };
+    Array.prototype.mapAsync = function(this, ...args) { return mapAsync(this, ...args); };
 }
