6e9923c213d02b486c805aaf15e06f35e6170c7a
Ivan Dudinov <ivan@zajno.com>
2021-06-29 17:46:23 +0300

    validators regex: allow linkedin url to miss http(s)

--- src/validation/validators.ts
+++ src/validation/validators.ts
@@ -23,7 +23,7 @@ export const ValidatorsRegExps = {
     cvv: /^\d+$/,
     // eslint-disable-next-line no-useless-escape
     website: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
-    linkedin: /^https?:\/\/.*linkedin\.com\/.*$/i,
+    linkedin: /^(https?:\/\/.*)?linkedin\.com\/.*$/i,
 };
 
 export const Validators = {
