68ecec766bb1e67aac3402a3260778a37829f36f
kiryaremchuk <k.yaremchuk@zajno.com>
2021-10-22 13:07:13 +0300

    [helpers] added firebase-logger & raised version

---
+++ src/helpers/logger.ts
@@ -0,0 +1,4 @@
+import { ILogger } from '@zajno/common/lib/logger';
+import { logger } from 'firebase-functions';
+
+export const firebaseLogger: ILogger = { log: logger.log.bind(logger), warn: logger.warn.bind(logger), error: logger.error.bind(logger) };
