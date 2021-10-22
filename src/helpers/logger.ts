77b6d2134dff3739fd7ba75874998a481c2cc0d9
Ivan Dudinov <ivan@zajno.com>
2021-10-22 13:57:27 +0300

    moved logger, minor styling fixes

--- src/helpers/logger.ts
+++
@@ -1,4 +0,0 @@
-import { ILogger } from '@zajno/common/lib/logger';
-import { logger } from 'firebase-functions';
-
-export const firebaseLogger: ILogger = { log: logger.log.bind(logger), warn: logger.warn.bind(logger), error: logger.error.bind(logger) };
