69573b16a25ba953186bfc40c39877cbf51f2437
DKlushnik <d.klushnik@zajno.com>
2021-11-29 15:10:54 +0200

    fixed isValueSelected in MultiSelectModel, updated test

--- src/viewModels/__tests__/multiSelect.test.ts
+++ src/viewModels/__tests__/multiSelect.test.ts
@@ -9,15 +9,21 @@ describe('MultiSelectModel', () => {
         const accessor = (i: number) => `_${i}_`;
         const values = items.map(accessor);
         const initialSelected = [1];
+        const selectedValues = [accessor(2)];
 
         const vm = new MultiSelect(items, accessor, ...initialSelected);
 
         expect(vm.items).toStrictEqual(items);
         expect(vm.values).toStrictEqual(values);
         expect(vm.selectedIndexes).toStrictEqual(initialSelected);
+        expect(vm.selectedValues).toStrictEqual(selectedValues);
 
         initialSelected.forEach(index => {
             expect(vm.isIndexSelected(index)).toBe(initialSelected.includes(index));
+        });
+
+        values.forEach((value) => {
+            expect(vm.isValueSelected(value)).toBe(selectedValues.includes(value));
         });
     });
 });
