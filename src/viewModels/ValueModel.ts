37bb7dfcd56caed2057b4041ee9a584c01fe78eb
Ivan Dudinov <ivan@zajno.com>
2022-02-15 23:38:18 +0200

    [viewModels] added ValueModel - simple and universal

--- src/viewModels/ValueModel.ts
+++ src/viewModels/ValueModel.ts
@@ -1,27 +1,21 @@
 import { action, makeObservable, observable } from 'mobx';
 import { Getter } from '../types';
-import { ValidatableModel } from './Validatable';
+import { IValueModel } from './types';
 
-export class CommonModel<T = any> extends ValidatableModel<T> {
-
+export class ValueModel<T> implements IValueModel<T> {
     // observable[.ref]
     private _value: T;
 
     private _defaultValue: Getter<T> = null;
 
     constructor(v: Getter<T> = null, useRefObservable = true) {
-        super();
         this._value = Getter.getValue(v) || null;
         this._defaultValue = v;
 
-        makeObservable<CommonModel<T>, '_value'>(this, {
+        makeObservable<ValueModel<T>, '_value'>(this, {
             _value: useRefObservable ? observable.ref : observable,
             setValue: action,
         });
-    }
-
-    protected get valueToValidate(): Readonly<T> {
-        return this._value;
     }
 
     public get value() { return this._value; }
@@ -32,14 +26,9 @@ export class CommonModel<T = any> extends ValidatableModel<T> {
     // action
     public readonly setValue = (value: T) => {
         this._value = value;
-
-        if (this._validateOnChange) {
-            this.validate();
-        }
     };
 
     reset = () => {
         this._value = Getter.getValue(this._defaultValue);
-        super.reset();
     };
 }
--- src/viewModels/index.ts
+++ src/viewModels/index.ts
@@ -1,12 +1,15 @@
 export * from './types';
 export * from './Validatable';
 export * from './CommonModel';
+export * from './ValueModel';
+
 export * from './TextModel';
 export * from './FlagModel';
 export * from './NumberModel';
-export * from './wrappers';
 
 export * from './SelectModel';
 export * from './MultiSelectModel';
 
 export * from './PromptModal';
+
+export * from './wrappers';
