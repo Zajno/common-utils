c3093d41a0393ea983f511076e02968bcdc8f78d
Ivan Dudinov <ivan@zajno.com>
2022-04-12 19:26:13 +0100

    [viewModels] LoadingModel: updated overload typings

--- src/viewModels/LoadingModel.ts
+++ src/viewModels/LoadingModel.ts
@@ -4,6 +4,10 @@ export class LoadingModel extends NumberModel {
 
     public get isLoading() { return this.value > 0; }
 
+    public async useLoading<T>(cb: () => (T | Promise<T>)): Promise<T>;
+    public async useLoading<T>(cb: () => (T | Promise<T>), exclusive: false): Promise<T>;
+    public async useLoading<T>(cb: () => (T | Promise<T>), exclusive: true | 'throw'): Promise<T | false>;
+
     public async useLoading<T>(cb: () => (T | Promise<T>), exclusive: boolean | 'throw' = false): Promise<T | false> {
         if (exclusive && this.isLoading) {
             if (exclusive === 'throw') {
