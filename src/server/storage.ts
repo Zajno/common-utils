09c65471cc42378a22c565e656f10f09fc35be1f
Roman Nikitin <68279389+rnikitin1@users.noreply.github.com>
2023-06-23 14:11:49 +0300

    [server] added delete method for storage files (#6)

--- src/server/storage.ts
+++ src/server/storage.ts
@@ -14,3 +14,26 @@ export async function removeDirectoryFromStorage(path: string): Promise<void> {
         logger.error(`Error while deleting files :::: ${path} :::: ${error}`);
     }
 }
+
+export async function deleteFile(fileRef: string) {
+    const file = StorageContext.bucket.file(fileRef);
+    try {
+        logger.log('Deleting file by ref:', fileRef);
+        await file.delete();
+        return true;
+    } catch (err) {
+        logger.error('Failed to delete file by ref', fileRef, ';\r\nERROR:', err);
+        return false;
+    }
+}
+
+export async function deleteAllFilesIn(targetBucket, path: string) {
+    logger.log(`Deleting all files by path: ${targetBucket.name}/${path}`);
+    const [files] = await targetBucket.getFiles({ prefix: path });
+    await Promise.all(
+        files.map(async file => {
+            const filePath = file.name;
+            await targetBucket.file(filePath).delete();
+        })
+    );
+}
