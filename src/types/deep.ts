b244816c05997ede7eaec78839753e697d53cefe
Ivan Dudinov <ivan@zajno.com>
2023-06-29 22:14:41 +0100

    [types] update Deep* types constraint, generic arguments are to extend Object

--- src/types/deep.ts
+++ src/types/deep.ts
@@ -1,18 +1,38 @@
 
 type Primitive = number | string | Symbol | boolean | null | undefined | bigint;
 
-export type DeepReadonly<T> = T extends (Function | Primitive)
-    ? T
-    : (
-        T extends Object
-            ? { readonly [P in keyof T]: DeepReadonly<T[P]>; }
-            : T
-    );
+export type DeepReadonly<T extends Object> = {
+    readonly [P in keyof T]: T[P] extends (Function | Primitive)
+        ? T[P]
+        : (T[P] extends Object
+            ? DeepPartial<T[P]>
+            : T[P]
+        );
+};
 
-export type DeepPartial<T> = T extends (Function | Primitive)
-    ? T
-    : (
-        T extends Object
-            ? { [P in keyof T]?: DeepPartial<T[P]>; }
-            : T
-    );
+export type DeepPartial<T extends Object> = {
+    [P in keyof T]?: T[P] extends (Function | Primitive)
+        ? T[P]
+        : (T[P] extends Object
+            ? DeepPartial<T[P]>
+            : T[P]
+        );
+};
+
+export type DeepRequired<T extends Object> = {
+    [P in keyof T]-?: T[P] extends (Function | Primitive)
+        ? T[P]
+        : (T[P] extends Object
+            ? DeepRequired<T[P]>
+            : T[P]
+        );
+};
+
+export type DeepMutable<T extends Object> = {
+    -readonly [P in keyof T]: T[P] extends (Function | Primitive)
+        ? T[P]
+        : (T[P] extends Object
+            ? DeepMutable<T[P]>
+            : T[P]
+        );
+};
