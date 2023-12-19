c2e23899c276fea193a29c11fe6661cc7df3c464
Ivan Dudinov <ivan@zajno.com>
2023-12-19 11:42:00 +0000

    [common] PromiseCache batching

--- packages/common/src/structures/__tests__/promiseCache.test.ts
+++ packages/common/src/structures/__tests__/promiseCache.test.ts
@@ -1,8 +1,17 @@
 
 import { setTimeoutAsync } from '../../async/timeout';
-import { PromiseCache } from '../promiseCache';
+import { ILogger } from '../../logger';
+import { DeferredGetter, PromiseCache } from '../promiseCache';
 
 describe('PromiseCache', () => {
+
+    it('Empty Deferred Getter', async () => {
+        expect(DeferredGetter.Empty.current).toBeNull();
+        expect(DeferredGetter.Empty.busy).toBe(false);
+
+        await expect(DeferredGetter.Empty.promise).resolves.toBeNull();
+    });
+
     it('hard load', async () => {
 
         const COUNT = 1000;
@@ -31,5 +40,146 @@ describe('PromiseCache', () => {
         expect(busyCount).toBe(COUNT);
 
         await expect(cache.getDeferred(TEST_ID).promise).resolves.toBe(TEST_OBJ);
+    });
+
+    it('infrastructure', async () => {
+        const loaderFn = vi.fn();
+        const TEST_OBJ = { HELLO: 'WORLD' };
+
+        const getRes = (id: string | number) => ({ ...TEST_OBJ, id });
+
+        const fetcher = async (id: string | number) => {
+            loaderFn();
+            await setTimeoutAsync(200);
+            return getRes(id);
+        };
+
+        const cacheNoAdapters = new PromiseCache(fetcher);
+
+        expect(() => cacheNoAdapters.getCurrent(null as any)).toThrow();
+        expect(() => cacheNoAdapters.getCurrent(123)).toThrow();
+        expect(cacheNoAdapters.keysParsed()).toBeNull();
+
+        const cache = new PromiseCache(
+            fetcher,
+            id => id.toString(),
+            id => +id,
+        );
+
+        await expect(cache.get(123)).resolves.toStrictEqual({ ...TEST_OBJ, id: 123 });
+        expect(loaderFn).toHaveBeenCalledTimes(1);
+
+        expect(cache.keys()).toStrictEqual(['123']);
+        cache.invalidate(123);
+        expect(cache.keys()).toStrictEqual([]);
+
+        loaderFn.mockClear();
+
+        const batchLoaderFn = vi.fn();
+        const batchLoader = async (ids: number[]) => {
+            batchLoaderFn();
+            await setTimeoutAsync(100);
+            return ids.map(getRes);
+        };
+
+        cache.useBatching(batchLoader);
+
+        const filler = new Array<number>(5).fill(0, 0, 5);
+
+        const results = Promise.all(
+            filler.map(async (_, i) => {
+                setTimeoutAsync(10 * i);
+                return cache.get(i);
+            }),
+        );
+
+        expect(loaderFn).not.toHaveBeenCalled();
+
+        await expect(results).resolves.toStrictEqual(
+            filler.map((_, i) => getRes(i)),
+        );
+
+        filler.forEach((_, i) => {
+            expect(cache.hasKey(i)).toBe(true);
+            expect(cache.getCurrent(i)).toStrictEqual(getRes(i));
+        });
+
+        expect(batchLoaderFn).toHaveBeenCalledTimes(1);
+
+        expect(cache.getCurrent(1)).toStrictEqual(getRes(1));
+
+        expect(cache.keys()).toStrictEqual(filler.map((_, i) => i.toString()));
+        expect(cache.keysParsed()).toStrictEqual(filler.map((_, i) => i));
+
+        cache.invalidate(1);
+
+        expect(cache.hasKey(1)).toBe(false);
+
+        cache.updateValueDirectly(1, getRes(1));
+        expect(cache.hasKey(1)).toBe(true);
+
+        const def = cache.getDeferred(1);
+        expect(def.current).not.toBeUndefined();
+        expect(def.busy).toBeFalsy();
+        await expect(def.promise).resolves.toStrictEqual(getRes(1));
+    });
+
+    it('batching fails', async () => {
+
+        const loaderFn = vi.fn();
+        const TEST_OBJ = { HELLO: 'WORLD' };
+
+        const getRes = (id: string | number) => ({ ...TEST_OBJ, id });
+
+        const fetcher = async (id: string | number) => {
+            loaderFn();
+            await setTimeoutAsync(200);
+            return getRes(id);
+        };
+
+        const cache = new PromiseCache(
+            fetcher,
+            id => id.toString(),
+            id => +id,
+        );
+
+        const batchError = new Error('Batching failed in test');
+
+        const batchLoaderFn = vi.fn();
+        const batchLoader = async (_ids: number[]) => {
+            batchLoaderFn();
+            await setTimeoutAsync(100);
+            throw batchError;
+        };
+
+        cache.useBatching(batchLoader);
+
+        const logger: ILogger = {
+            log: vi.fn(),
+            error: vi.fn(),
+            warn: vi.fn(),
+        };
+        cache.useLogger(logger);
+
+        const filler = new Array<number>(5).fill(0, 0, 5);
+
+        const results = Promise.all(
+            filler.map(async (_, i) => {
+                setTimeoutAsync(10 * i);
+                return cache.get(i);
+            }),
+        );
+
+        await expect(results).resolves.toStrictEqual(
+            filler.map((_, i) => getRes(i)),
+        );
+
+        expect(batchLoaderFn).toHaveBeenCalledTimes(1);
+        expect(loaderFn).toHaveBeenCalledTimes(5);
+
+        expect(logger.warn).toHaveBeenCalledTimes(5);
+        for (let i = 0; i < 5; ++i) {
+            expect(logger.warn).toHaveBeenCalledWith('batch fetch failed', i, batchError);
+        }
     });
 });
