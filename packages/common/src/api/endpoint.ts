85bc881b2fcbb0a7afbe66b27a8a933e447022f1
Ivan Dudinov <ivan@kalmstead.com>
2024-05-03 17:26:54 +0100

    doc comments updates

--- packages/common/src/api/endpoint.ts
+++ packages/common/src/api/endpoint.ts
@@ -44,18 +44,22 @@ IEndpointInfo.IHeaders<THeaders> {
         return res;
     }
 
+    /** Creates an instance and applies 'GET' method for it. */
     static get<TOut>(displayName?: string) {
         return ApiEndpoint.construct<null, TOut>(EndpointMethods.GET, displayName);
     }
 
+    /** Creates an instance and applies 'POST' method for it. */
     static post<TIn extends object | null, TOut = void>(displayName?: string) {
         return ApiEndpoint.construct<TIn, TOut>(EndpointMethods.POST, displayName);
     }
 
+    /** Creates an instance and applies 'DELETE' method for it. */
     static delete<TOut>(displayName?: string) {
         return ApiEndpoint.construct<null, TOut>(EndpointMethods.DELETE, displayName);
     }
 
+    /** Applies a type based on {@link Path.IBuilder} type by accepting arguments for {@link Path.construct} function */
     public withPath<P extends Path.BaseInput[]>(...path: P): ApiEndpoint<TIn, TOut, Path.ExtractArgs<Path.CombineBuilders<P>>, TQuery, TErrors, THeaders> {
         type PathType = Path.CombineBuilders<P>;
         type Args = Path.ExtractArgs<PathType>;
@@ -65,22 +69,29 @@ IEndpointInfo.IHeaders<THeaders> {
         return res;
     }
 
+    /** Applies query type and also store query keys to make api caller be able to distinguish which keys should be ejected from request body. */
     public withQuery<TQ extends object>(...queryKeys: (string & keyof TQ)[]): ApiEndpoint<TIn, TOut, TPath, TQ, TErrors, THeaders> {
         const res = this as unknown as ApiEndpoint<TIn, TOut, TPath, TQ, TErrors, THeaders>;
         res.queryKeys = queryKeys;
         return res;
     }
 
+    /** Applies error type, optionally stores error processor. */
     public withErrors<TErr>(errorProcessor?: (err: TErr) => void): ApiEndpoint<TIn, TOut, TPath, TQuery, TErr, THeaders> {
         const res = this as unknown as ApiEndpoint<TIn, TOut, TPath, TQuery, TErr, THeaders>;
         res.errorProcessor = errorProcessor;
         return res;
     }
 
+    /** Applies headers type. */
     public withHeaders<THeads>(_headersMarker?: THeads): ApiEndpoint<TIn, TOut, TPath, TQuery, TErrors, THeads> {
         return this as unknown as ApiEndpoint<TIn, TOut, TPath, TQuery, TErrors, THeads>;
     }
 
+    /** Marks this instance as one has to be sent as form.
+     *
+     * TODO: introduce more content types support.
+     */
     public asForm() {
         this.isForm = true;
         return this;
