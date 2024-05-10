f5d68b6302558231a3987c7efa380479e170d8c4
Ivan Dudinov <ivan@kalmstead.com>
2024-05-10 12:06:06 +0100

    [common] PromiseExtended: safe get promise (#72)
    * [common] PromiseExtended: safe getting promise from callback
    
    * [common] api: fixed import
    
    * [common-mobx] fixed DateViewModel type
    
    * [common, common-mobx] raised versions

--- packages/common-mobx/src/viewModels/DateViewModel.ts
+++ packages/common-mobx/src/viewModels/DateViewModel.ts
@@ -1,7 +1,7 @@
 import { Nullable } from '@zajno/common/types';
 import { CommonModel } from './CommonModel';
 
-export class DateViewModel extends CommonModel<Date> {
+export class DateViewModel extends CommonModel<Date | null> {
 
     private _min: Nullable<Date> = null;
     private _max: Nullable<Date> = null;
