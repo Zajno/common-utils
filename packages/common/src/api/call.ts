4e3c6f4ce8fb6f8115e4e8180ad3dc315df17635
Ivan Dudinov <ivan@kalmstead.com>
2024-06-02 09:18:48 +0100

    [common] API: configurable extra arguments for caller

--- packages/common/src/api/call.ts
+++ packages/common/src/api/call.ts
@@ -5,6 +5,7 @@ import { EndpointMethods } from './methods';
 import { LogTypes } from './logging';
 import { getPath } from './helpers';
 
+/** Request options to be used by call implementation (e.g. interceptor). Passed as separate object argument to the call method.  */
 type Extra<T> = {
     headers?: IEndpointInfo.ExtractHeaders<T>;
     log?: LogTypes<IEndpointInfo.ExtractIn<T>, IEndpointInfo.ExtractOut<T>>;
@@ -18,34 +19,50 @@ export type RequestConfig<TIn> = {
     headers: AnyObject;
 };
 
-export type RequestConfigDetails<T extends IEndpointInfo = IEndpointInfo, TIn = IEndpointInfo.ExtractIn<T>> = RequestConfig<TIn> & {
+/** Compiled request config object passed to `request` implementation */
+export type RequestConfigDetails<
+    T extends IEndpointInfo = IEndpointInfo,
+    TIn = IEndpointInfo.ExtractIn<T>,
+    TExtra extends object = Record<string, any>
+> = RequestConfig<TIn> & {
     _api: T;
     _noLoader?: boolean;
     _log?: LogTypes;
+    /** Additional data passed to the request implementation */
+    _extra?: TExtra;
 };
 
-type CallerOptions = {
+type CallerOptions<TExtra extends object = Record<string, any>> = {
+    /** Additional hook before hading request config to implementation */
     bodyValidation?: (api: IEndpointInfo, input: any) => Promise<void>;
-    request: <TIn, TOut>(config: RequestConfigDetails<IEndpointInfo, TIn>) => Promise<{ data: TOut }>;
+
+    /** Request implementation */
+    request: <TIn, TOut>(config: RequestConfigDetails<IEndpointInfo, TIn, TExtra>) => Promise<{ data: TOut }>;
 };
 
 export type EndpointCallArgs<T extends IEndpointInfo> = IEndpointInfo.ExtractIn<T> | IEndpointInfo.ExtractPath<T> | IEndpointInfo.ExtractQuery<T>;
 
-export function buildApiCaller(options: CallerOptions) {
+export function buildApiCaller<TExtra extends object = Record<string, any>>(options: CallerOptions<TExtra>) {
 
     const { bodyValidation, request } = options;
 
     return async function callApi<T extends IEndpointInfo>(
         api: T,
         data: EndpointCallArgs<T>,
-        extra?: Extra<T>,
+        extra?: Extra<T> & TExtra
     ) {
 
         type TOut = IEndpointInfo.ExtractOut<T>;
+        type TIn = IEndpointInfo.ExtractIn<T>;
 
-        const { headers, log = 'res' } = extra || {};
+        const {
+            headers,
+            log = 'res',
+            noLoader,
+            ...restExtra
+        } = extra || {};
 
-        const resultInput = data && { ...data } as IEndpointInfo.ExtractIn<T>;
+        const resultInput = data && { ...data } as TIn;
         const pathInputs: Record<string, string | number> = {};
         let queryStr: string = '';
 
@@ -85,14 +102,16 @@ export function buildApiCaller(options: CallerOptions) {
             : undefined;
 
         const method = api.method || 'GET';
-        const config: RequestConfigDetails<T> = {
+        const config: RequestConfigDetails<T, TIn, TExtra> = {
             method,
             url: getPath(api, pathInputs as IEndpointInfo.ExtractPath<T>) + queryStr,
             data: PreProcessors.process(api, sendingData) || undefined,
             headers: headers as AnyObject || {},
             _api: api,
-            _noLoader: extra?.noLoader == null ? method as string === 'GET' : extra.noLoader,
+
+            _noLoader: noLoader ?? method as string === 'GET',
             _log: log,
+            _extra: restExtra as TExtra,
         };
 
         if (api.isForm) {
