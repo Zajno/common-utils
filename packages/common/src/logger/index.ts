2087b4b3a952576dc3738b63077ace45a4cf9969
Ivan Dudinov <ivan@kalmstead.com>
2024-09-12 22:08:43 +0100

    added logger manager

--- packages/common/src/logger/index.ts
+++ packages/common/src/logger/index.ts
@@ -1,72 +1,28 @@
-import { Getter } from '../types';
 import { ILogger, LoggerFunction } from './abstractions';
-import { CONSOLE, ConsoleLogger } from './console';
-import { ProxyLogger } from './proxy';
+import { ConsoleLogger } from './console';
 import { batchLoggers } from './batch';
-import { removeItem } from '../math/arrays';
+import { LoggerModes, LoggersManager } from './manager';
 
 export type { ILogger, LoggerFunction };
 export { NamedLogger } from './named';
 export { ConsoleLogger, batchLoggers };
 
-// TBD Introduce more logger types ?
-export type LoggerTypes = 'console';
+const manager = new LoggersManager();
 
-let Mode: LoggerTypes | false | Getter<ILogger> = false;
-
-const proxies: ProxyLogger[] = [];
-
-export function createLogger(name: string | undefined, mode: undefined | typeof Mode = undefined): ILogger {
-    const result = _createImplementation(mode);
-    const proxy = new ProxyLogger(result, name);
-    proxies.push(proxy);
-    return proxy;
+export function createLogger(name: string | undefined, mode: undefined | LoggerModes = undefined): ILogger {
+    return manager.create(name, mode);
 }
 
 export function detachLogger(instance: ILogger, terminate = false) {
-    const item = removeItem(proxies, instance as ProxyLogger);
-    if (item) {
-        if (terminate) {
-            item.disable();
-        }
-        return true;
-    }
-    return false;
+    return manager.detach(instance, terminate);
 }
 
-export function setMode(mode: typeof Mode | null | undefined) {
-    if (Mode === mode) {
-        return;
-    }
-
-    Mode = mode || false;
-
-    if (!Mode) {
-        proxies.forEach(l => l.disable());
-    } else {
-        proxies.forEach(l => l.setLogger(_createImplementation()));
-    }
+export function setMode(mode: LoggerModes | null | undefined) {
+    manager.setMode(mode);
 }
 
-export function getMode() { return Mode; }
+export function getMode() { return manager.mode; }
 
 const logger: ILogger = createLogger(undefined, false);
 
 export default logger;
-
-function _createImplementation(overrideMode: undefined | typeof Mode = undefined): ILogger | null {
-    const mode = overrideMode !== undefined ? overrideMode : Mode;
-    switch (mode) {
-        case 'console': {
-            return CONSOLE;
-        }
-
-        case false: {
-            return null;
-        }
-
-        default: {
-            return Getter.getValue(mode);
-        }
-    }
-}
