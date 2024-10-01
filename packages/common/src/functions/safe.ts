dc7b0ea6e9d325c6e312e3da4d34de98019b7211
Ivan Dudinov <ivan@kalmstead.com>
2024-10-01 15:17:14 +0100

    [common] `wrapAsync`: wrap a promise-returning fn to be a void-returning (#88)
    * build: fixed `build:w` script
    
    * assert: use new `Getter` methods
    
    * functions/safe: introduce `wrapAsync` for generating non-Promise callback
    
    * wrapAsync: null callback handling
    
    * bumped version
    
    * wrapAsync/catchPromise: laxer types

--- packages/common/src/functions/safe.ts
+++ packages/common/src/functions/safe.ts
@@ -1,4 +1,15 @@
+import { Nullable } from '../types';
 
-export function catchPromise(promise: Promise<any> | void, cb?: (err: any) => void) {
+type LaxPromise<T> = Promise<T> | T | void;
+
+export function catchPromise(promise: LaxPromise<any>, cb?: (err: any) => void) {
     Promise.resolve(promise).catch(err => cb?.(err));
+}
+
+export function wrapAsync<T, TArgs extends any[]>(fn: Nullable<(...args: TArgs) => LaxPromise<T>>, cb?: (err: any) => void): (...args: TArgs) => void {
+    return (...args: TArgs) => {
+        if (fn) {
+            catchPromise(fn(...args), cb);
+        }
+    };
 }
