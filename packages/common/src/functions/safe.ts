c3636d20c75e6114c26291ca979188ffb1156a17
Ivan Dudinov <ivan@kalmstead.com>
2024-10-01 11:19:14 +0100

    functions/safe: introduce `wrapAsync` for generating non-Promise callback

--- packages/common/src/functions/safe.ts
+++ packages/common/src/functions/safe.ts
@@ -2,3 +2,9 @@
 export function catchPromise(promise: Promise<any> | void, cb?: (err: any) => void) {
     Promise.resolve(promise).catch(err => cb?.(err));
 }
+
+export function wrapAsync<T, TArgs extends any[]>(fn: (...args: TArgs) => Promise<T>, cb?: (err: any) => void): (...args: TArgs) => void {
+    return (...args: TArgs) => {
+        catchPromise(fn(...args), cb);
+    };
+}
