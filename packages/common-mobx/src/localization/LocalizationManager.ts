48f0db5d0c48c2851c861bf20f9a507561a36db9
Ivan Dudinov <ivan@kalmstead.com>
2025-04-04 12:47:04 +0100

    [common-mobx] updated observable LocalizationManager

--- packages/common-mobx/src/localization/LocalizationManager.ts
+++ packages/common-mobx/src/localization/LocalizationManager.ts
@@ -1,19 +1,20 @@
 import { observable, makeObservable, action } from 'mobx';
 import type { AnyObject } from '@zajno/common/types';
-import { LocalizationManager as _LocalizationManager } from '@zajno/common/localization';
+import { LocalizationManager as _LocalizationManager, StringsDataSource } from '@zajno/common/localization';
 
 export class LocalizationManager<TLocaleType extends string, TStrings extends AnyObject> extends _LocalizationManager<TLocaleType, TStrings> {
     constructor(
-        dataSource: { [locale: string]: TStrings },
+        dataSource: StringsDataSource<TLocaleType, TStrings>,
         initialLocale: TLocaleType,
-        defaultLocale: TLocaleType | null = null,
+        fallbackStrings: TStrings | null = null,
     ) {
-        super(dataSource, initialLocale, defaultLocale);
+        super(dataSource, initialLocale, fallbackStrings);
 
-        makeObservable<LocalizationManager<TLocaleType, TStrings>, '_currentLocale' | '_currentStrings'>(this, {
+        makeObservable<LocalizationManager<TLocaleType, TStrings>, '_currentLocale' | '_currentStrings' | 'doUpdateStrings'>(this, {
             _currentLocale: observable,
             _currentStrings: observable.ref,
             useLocale: action,
+            doUpdateStrings: action,
         });
     }
 }
