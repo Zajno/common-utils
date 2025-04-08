052ff8c1e36341d4c467479529a43cdea4b833b0
Ivan Dudinov <ivan@kalmstead.com>
2025-04-08 14:45:44 +0100

    fixed tz `getOffset`, now accepting target date to be able to work with DST/no-DST difference

--- packages/common/src/dates/tz.ts
+++ packages/common/src/dates/tz.ts
@@ -1,19 +1,23 @@
-
-/** This is experimental stuff, so no tests provided, see comments in the code */
+/** Helpers for time zones, based on `Intl.DateTimeFormat` */
 export namespace Timezones {
 
     export const Current = Intl.DateTimeFormat().resolvedOptions().timeZone;
     export const CurrentOffset = new Date().getTimezoneOffset();
 
     // https://stackoverflow.com/a/64262840/9053142
-    // tested locally, but it's hard to write tests since they are based on the local timezone, browser implementation and whether DST is active
-    // it's recommended to wrap with try/catch and don't rely it will always work
-    export const getOffset = (timeZone: string) => {
+    /**
+     * Get the offset in minutes for a given time zone and date. Uses Intl.DateTimeFormat
+     * to get the time zone name, then parses it to get the offset.
+     * @param timeZone time zone name, e.g. 'America/New_York', 'Europe/Kyiv', etc.
+     * @param date date to get the offset for. If not provided, uses the current date.
+     * @returns offset in minutes. Positive values are west of UTC, negative values are east of UTC.
+     */
+    export function getOffset(timeZone: string, date?: Date | number) {
         const timeZoneName = Intl.DateTimeFormat('ia', {
             timeZoneName: 'longOffset',
             timeZone,
         })
-            .formatToParts()
+            .formatToParts(date)
             .find((i) => i.type === 'timeZoneName')
             ?.value;
 
@@ -37,18 +41,18 @@ export namespace Timezones {
         }
 
         return result;
-    };
+    }
 
-    // Tests may look like this:
-    /*
-        expect(Timezones.getOffset('America/New_York')).toBe(240);
-        expect(Timezones.getOffset('Europe/Paris')).toBe(-120);
-        expect(Timezones.getOffset('Europe/Kiev')).toBe(-180);
-        expect(Timezones.getOffset('Asia/Kolkata')).toBe(-270);
+    /** Returns a new date with the specified timezone offset applied.
+     *
+     * Usage case can be imagined like this: if your local wall clock shows time X,
+     * what your clock will show when another local wall clock in the specified timezone will be showing time X.
     */
-
     export function shiftToTimeZone(date: Date, tz: string | number): Date {
-        const offset = typeof tz === 'number' ? tz : getOffset(tz);
+        const offset = typeof tz === 'number'
+            ? tz
+            : getOffset(tz, date);
+
         const shifted = new Date(date.getTime() + offset * 60 * 1000);
         return shifted;
     }
