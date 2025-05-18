aed318f08aed1d4eaed08fe4c514f3ab0def0906
Ivan Dudinov <ivan@kalmstead.com>
2025-05-18 12:58:02 +0100

    [common] localization: fixed exports, added `synchronizeLocale` helper

--- packages/common-mobx/src/localization/index.ts
+++ packages/common-mobx/src/localization/index.ts
@@ -1,6 +1,6 @@
 import { LocalizationManager } from './LocalizationManager.js';
 import { LocalizedValidationErrors } from './LocalizedValidationErrors.js';
 
-export * from '@zajno/common/localization/abstractions.js';
+export type * from '@zajno/common/localization/abstractions.js';
 
 export { LocalizationManager, LocalizedValidationErrors };
--- packages/common/src/localization/index.ts
+++ packages/common/src/localization/index.ts
@@ -1,6 +1,6 @@
 import { LocalizationManager } from './LocalizationManager.js';
 import { LocalizedValidationErrors } from './LocalizedValidationErrors.js';
 
-export * from './abstractions.js';
+export type * from './abstractions.js';
 
 export { LocalizationManager, LocalizedValidationErrors };
