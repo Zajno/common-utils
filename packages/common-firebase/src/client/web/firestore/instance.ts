1f33c06bf7cd7f0071947da1b2c2af0f78f0ad3e
Ivan Dudinov <ivan@kalmstead.com>
2025-09-28 15:58:58 +0100

    fixed firestore initialization

--- packages/common-firebase/src/client/web/firestore/instance.ts
+++ packages/common-firebase/src/client/web/firestore/instance.ts
@@ -2,29 +2,20 @@ import { FirebaseApp, createFirebaseLazy } from '../app.js';
 import {
     type FirestoreSettings,
     initializeFirestore,
-    getFirestore,
 } from 'firebase/firestore';
 
 export const Firestore = createFirebaseLazy(() => {
     try {
-        if (FirebaseApp.Settings) {
-            const settings: FirestoreSettings = { ...FirebaseApp.Settings.firestore };
-            if (settings.host) {
-                FirebaseApp.logger?.log('Firestore will use emulator: ', settings.host);
-                settings.ssl = false;
-            }
-
-            initializeFirestore(FirebaseApp.Current, settings);
+        const settings: FirestoreSettings = { ...FirebaseApp.Settings?.firestore };
+        if (settings.host) {
+            FirebaseApp.logger?.log('Firestore will use emulator: ', settings.host);
+            settings.ssl = false;
         }
 
         const databaseId = FirebaseApp.Settings?.config?.firestoreDatabaseId || undefined;
+        FirebaseApp.logger?.log('creating Firestore... for app = ', FirebaseApp.Current.options.projectId, '; databaseId = ', databaseId, '; settings = ', settings);
 
-        FirebaseApp.logger?.log('creating Firestore... for app = ', FirebaseApp.Current.options.projectId, '; databaseId = ', databaseId);
-        if (databaseId) {
-            return getFirestore(FirebaseApp.Current, databaseId);
-        }
-
-        return getFirestore(FirebaseApp.Current);
+        return initializeFirestore(FirebaseApp.Current, settings, databaseId);
     } catch (err) {
         FirebaseApp.logger?.error('Failed to create Firestore:', err);
         return null!; // TODO better re-throw?
