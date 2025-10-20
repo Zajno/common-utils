ea9e9d6b9f5b2b5fe26cdf8c0bbbb809f637c8e1
Ivan Dudinov <ivan@kalmstead.com>
2025-10-20 13:10:17 +0100

    type fix

--- packages/common-firebase/src/client/abstractions/app.ts
+++ packages/common-firebase/src/client/abstractions/app.ts
@@ -1,11 +1,10 @@
 import type { ILogger } from '@zajno/common/logger';
 import type { FirebaseConfig } from '../../config.js';
-import type { Nullable } from '@zajno/common/types/misc.js';
 
 const EmptyAppSettings = {
     functionsEmulator: null! as { url: string },
     firestore: null! as {
-        host: Nullable<string>,
+        host: string | undefined,
         ignoreUndefinedProperties?: boolean,
         experimentalForceLongPolling?: boolean,
         experimentalAutoDetectLongPolling?: boolean,
